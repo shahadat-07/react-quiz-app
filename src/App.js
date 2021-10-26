937f2c727489e4d71dfcd1cc1600bb1d177af6fa
Shahadat Hossen <shahadat.hossen.web@gmail.com>
2021-10-26 15:34:38 +0600

    Private and Public routes functionality are done

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import "./App.css";
 import Home from "./components/Home/Home/Home";
 import Layout from "./components/Layout/Layout";
 import Login from "./components/Login/Login";
+import PrivateRoute from "./components/PrivateRoute/PrivateRoute";
+import PublicRoute from "./components/PublicRoute/PublicRoute";
 import Quiz from "./components/Quiz/Quiz/Quiz";
 import Result from "./components/Result/Result/Result";
 import Signup from "./components/Signup/Signup/Signup";
@@ -13,19 +15,11 @@ function App() {
       <AuthProvider>
         <Layout>
           <Switch>
-            <Route exact path="/">
-              <Home />
-            </Route>
-            <Route path="/quiz">
-              <Quiz />
-            </Route>
-            <Route path="/signup">
-              <Signup />
-            </Route>
-            <Route path="/login">
-              <Login />
-            </Route>
-            <Result />
+            <Route exact path="/" component={Home} />
+            <PublicRoute exact path="/signup" component={Signup} />
+            <PublicRoute exact path="/login" component={Login} />
+            <PrivateRoute exact path="/quiz" component={Quiz} />
+            <PrivateRoute exact path="/result" component={Result} />
           </Switch>
         </Layout>
       </AuthProvider>
